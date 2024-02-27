bcff41e72b53ce3316b6aa0b39fbf4f8782e9502
studcl <134058542+studcl@users.noreply.github.com>
2024-02-27 20:59:39 +0800

    Update Monitor-install.sh

--- kubernetes/Monitor/Monitor-install.sh
+++ kubernetes/Monitor/Monitor-install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 ##VER=$(curl -s https://api.github.com/repos/weaveworks/scope/releases/latest|grep tag_name|cut -d '"' -f 4|sed 's/v//')
 ##kubectl apply -f https://github.com/weaveworks/scope/releases/download/v$VER/k8s-scope.yaml
-kubectl apply -f ./k8s-scope.yaml
+kubectl create ns weave
+DIR="$(cd "$(dirname "$0")" && pwd)"
+kubectl apply -f $DIR/k8s-scope.yaml
 ## Expose using NodePort ###
 kubectl --namespace weave patch svc weave-scope-app -p '{"spec": {"type": "NodePort"}}'
