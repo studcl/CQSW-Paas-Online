a647b7954cb7ea170d65c469480eb7493699273d
studcl <134058542+studcl@users.noreply.github.com>
2024-03-01 14:07:32 +0800

    Update Harbor-install.sh

--- docker/Harbor-install.sh
+++ docker/Harbor-install.sh
@@ -3,7 +3,9 @@ DIR="$(cd "$(dirname "$0")" && pwd)/../"
 source $DIR/cqsw-paas-config.sh
 Tar_DIR="$(cd "$(dirname "$0")" && pwd)"
 ##Install Docker Harbor
-wget https://github.com/goharbor/harbor/releases/download/v2.10.0/harbor-offline-installer-v2.10.0.tgz
+if [ ! -e "$Tar_DIR/harbor-offline-installer-v2.10.0.tgz" ]; then  
+            wget https://github.com/goharbor/harbor/releases/download/v2.10.0/harbor-offline-installer-v2.10.0.tgz  
+fi
 mv harbor-offline-installer-v2.10.0.tgz $Tar_DIR/harbor-offline-installer-v2.10.0.tgz
 tar -zxvf $Tar_DIR/harbor-offline-installer-v2.10.0.tgz
 Harbor_DIR="$(cd "$(dirname "$0")" && pwd)/harbor"
